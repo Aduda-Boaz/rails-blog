034b9a1666594728bb225961523f2011d105fd55
Aduda-Boaz <adudaboaz@gmail.com>
2022-07-18 23:34:24 +0300

    Update the route file

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "users#index"
 
-  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
+  get '/users/:user_id/posts', to: 'posts#index', as: :users_posts
   get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  get '/users', to: 'users#index', as: :users
+  get '/users/:id', to: 'users#show', as: :user
 end
