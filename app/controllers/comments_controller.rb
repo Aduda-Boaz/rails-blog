00b8df0fdf621bdfb8cd7f873f292c8d2c189b32
Aduda-Boaz <adudaboaz@gmail.com>
2022-07-22 10:05:05 +0300

    Modify the post controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @user = current_user
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      redirect_to user_post_path(@comment.post_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,20 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.where(author_id: params[:user_id])
-    @comments = Comment.all
-    @users = User.all
-  end
+  @posts = Post.where(author_id: params[:user_id])
+  @comments = Comment.all
+  @users = User.all
 
-  def show
-    @post = Post.find(params[:id])
-    @users = User.all
-    @comments = Comment.all
-  end
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_count, :likes_count, numericality: { only_integer: true }
 
-  def new
-    @post = Post.new
-    @user = current_user
-  end
+  after_save :update_post_count
 
-  def create
-    @post = Post.new(post_params)
-    @post.author_id = current_user.id
-    if @post.save
-      redirect_to user_posts_path(current_user)
-    else
-      render :new
-    end
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 
   private
 
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def update_post_count
+    user.increment!(:post_count)
   end
 end
