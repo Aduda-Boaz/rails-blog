cd77cd2b2cb386457fe34a5a1ccdec8f63d0c034
Aduda-Boaz <adudaboaz@gmail.com>
2022-07-22 10:07:20 +0300

    Update the like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post
 
+  after_save :update_post_likes_count
+
+  private
+
   def update_post_likes_count
     post.increment!(:likes_count)
   end
