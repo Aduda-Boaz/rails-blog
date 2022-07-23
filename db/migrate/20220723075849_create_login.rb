12925d39c8cf1efc4a9ea6aa196f9f4488546b90
Aduda-Boaz <adudaboaz@gmail.com>
2022-07-23 13:05:07 +0300

    Migration errors fixed

--- db/migrate/20220723075849_create_login.rb
+++ db/migrate/20220723075849_create_login.rb
@@ -1,6 +1,6 @@
 class CreateLogin < ActiveRecord::Migration[7.0]
   def change
-    create_table :logins do |t|
+    create_table :login do |t|
       t.string :username
       t.string :password_digest
 
